746e2973b2c26e0351fb921a43ecc82e25aca468
greenxiaye <greenxiaye@163.com>
2021-03-07 21:07:51 +0800

    feat:路由懒加载

--- vue-test-0/src/router/index.js
+++ vue-test-0/src/router/index.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../components/Home"
-import About from "../components/About"
-import User from "../components/User"
+// import Home from "../components/Home"
+// import About from "../components/About"
+// import User from "../components/User"
 const routerHistory = createWebHistory()
+const Home = () => import('../components/Home')
+const About = () => import('../components/About')
+const User = () => import('../components/User')
 // 路由数组
 const routes = [
     { path: '/', redirect: '/home' }, //    { path: '', redirect: '/home' },
-    // { path: '/', component: Home },
+    // { path: '/home', component: Home },
+    // { path: '/about/:aboutId', component: About },
+    // { path: '/user/:abc', component: User },
+
+    //方式一直接引用 
+    // { path: '/home', component: () => import('../components/Home') },
+    // { path: '/about/:aboutId', component: () => import('../components/About') },
+    // { path: '/user/:abc', component: () => import('../components/User') },
+
+    //方式二
     { path: '/home', component: Home },
     { path: '/about/:aboutId', component: About },
-    { path: '/user/:abc', component: User }
+    { path: '/user/:abc', component: User },
 ]
 
 const router = createRouter({
